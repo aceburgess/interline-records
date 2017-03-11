e189bec693ca3a6cba387ddc9e79a173efc90b25
Ace Burgess <burgess.ace@gmail.com>
2017-03-11 16:44:40 -0500

    Make design changes

--- interline-records/src/actions/index.js
+++ interline-records/src/actions/index.js
@@ -5,8 +5,12 @@ export const SELECT_ARTIST = 'SELECT_ARTIST';
 export const CHANGE_MODAL = 'CHANGE_MODAL';
 
 // const API_URL = 'http://local-dev-application.com:8000/artists/?format=json';
-const API_URL = 'http://local-dev-application.com:8000';
-const API_KEY = 'c2582285e11cc92a937dc85bf94b4964a909f40c';
+// const API_URL = 'http://local-dev-application.com:8000';
+// const API_KEY = 'c2582285e11cc92a937dc85bf94b4964a909f40c';
+
+// DEV SERVER seenonvhs.com
+const API_URL = 'https://seenonvhs.com';
+const API_KEY = '24d30374d0a32eb548953959ebe5a64dd3bbedd8';
 
 var headersObject = {mode: 'cors', Authentication: API_KEY};
 
--- interline-records/src/reducers/artists.js
+++ interline-records/src/reducers/artists.js
@@ -4,10 +4,10 @@ const initialState = {
 	data: [],
 	selectedArtist: {
     id: 1,
-    name: 'Ace Elijah',
-    photo:'http://local-dev-application.com:8000/media/artist-photos/lima.jpg',
+    name: 'Artists',
+    photo:'https://seenonvhs.com/media/artist-photos/artist-default.jpg',
     biography: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet ipsum dictum nunc ornare eleifend et non dolor. Duis ac dapibus enim, tincidunt vehicula eros. Mauris rutrum tellus vitae enim tristique, id sagittis nisi interdum. Proin pretium porttitor arcu in sodales. Donec vehicula dictum convallis. Pellentesque eu congue sem. Mauris pharetra sollicitudin rhoncus. Donec risus massa, fermentum sollicitudin elit id, euismod imperdiet sem. Morbi interdum augue.',
-    website:'http://aceelijah.com'
+    website:'https://aceelijah.com'
   },
   modalState: {
     title: 'About Us',
